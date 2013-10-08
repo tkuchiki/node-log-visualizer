35c18274b29777a1860067a38976a31c5b6fee30
tkuchiki <kuchiki.taku@gmail.com>
2013-10-08 18:03:52 +0900

    initial commit
    template engine -> swig
    public/{javascripts,stylesheets,images} -> public/{js,css,imgs}

---
+++ app.js
@@ -0,0 +1,45 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes  = require('./routes');
+var user    = require('./routes/user');
+var http    = require('http');
+var path    = require('path');
+var swig    = require('swig');
+var app     = express();
+
+app.engine('html', swig.renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', false);
+// To disable Swig's cache, do the following:
+swig.setDefaults({ cache: false });
+// NOTE: You should always cache templates in a production environment.
+// Don't leave both of these to `false` in production!
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
